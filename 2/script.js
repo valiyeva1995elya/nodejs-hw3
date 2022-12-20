aca2b3dc1af8a1d5a065355a948986f5ff454c8c
Valiyeva Elmira <valiyeva1995elya@gmail.com>
2022-12-20 09:17:50 +0600

    new func

--- 2/script.js
+++ 2/script.js
@@ -2,19 +2,19 @@ const addCarBtn = document.querySelector("#add_car_btn");
 const newCarNameInput = document.querySelector("#car_name");
 const carsListUl = document.querySelector("#cars_list");
 
-newCar(){
-    
-}
 
 const BASE_URL = "http://localhost:8080"
 const loadCarsList = async () => {
     const response = await fetch(BASE_URL + "/cars");
     const carsArray = await response.json();
-
-
-
     for(const {model} of carsArray){
         carsListUl.innerHTML +=`<li> ${model}</li>`
     }
 }
 loadCarsList()
+
+function newCar(){
+    let newModel = document.querySelector("#car_name").value;
+    carsListUl.innerHTML +=`<li> ${newModel}</li>`;
+    
+}
